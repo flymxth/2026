151a4fc9e1cf15848d23347e87b1e0681aa24d13
Luis Antonio Custodio Guzman <flymxth@MacBook-Air-de-Luis.local>
2026-05-29 13:58:59 -0600

    Pagina Personal

---
+++ BARBER/script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+toggle.addEventListener('click', () => {
+
+    menu.classList.toggle('active');
+
+    if(menu.classList.contains('active')){
+        toggle.textContent = '✕';
+    }
+
+    else {
+        toggle.textContent = '☰';
+    }
+});
